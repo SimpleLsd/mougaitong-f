be27b3fe029324bec5855af44de26689b1cfb754
SimpleLsd_MAC <liusungti@163.com>
2022-12-08 16:17:13 +0800

    12-8 主页顶部图片响应式拉取

--- src/components/layout/home/TopRecommend.vue
+++ src/components/layout/home/TopRecommend.vue
@@ -1,54 +1,72 @@
 <template>
   <div class="top_recommend">
+    <div
+      class="top_article_cover"
+      :style="{ backgroundImage: `url(${toparticle.cover})` }"
+    ></div>
     <div class="top_article">
-      <div class="cover">{{ topRecommendData.propA }}</div>
-      <div class="text">测试文字</div>
-      <h3>测试文字H3</h3>
-      <div class="inner">
-        <div class="small_title num">
-          <!-- {{ storeNum }} -->
-          {{ metadata.totalNum }}
-        </div>
-        <div class="title"></div>
-        <div class="tag_date"></div>
-        <div class="des">
-          <span></span>
-        </div>
+      <div class="small_title num">
+        <!-- {{ storeNum }} -->
+        {{ metadata.totalNum }}
+        {{ count }}
+      </div>
+      <div class="title"></div>
+      <div class="tag_date"></div>
+      <div class="des">
+        <span></span>
       </div>
     </div>
+
     <div class="top_collection">
       <div class="cover"></div>
       <div class="small_title"></div>
       <div class="title"></div>
     </div>
   </div>
-  <!-- <img src="http://43.142.29.240/static/cover_01.png" alt="1" /> -->
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
 const store = useStore();
 
-const topRecommendData = defineProps({
-  propA: Number,
-});
+// const topRecommendData = defineProps({
+//   propA: Number,
+// });
 
 const metadata = computed(() => store.state.metadata);
+const toparticle = computed(() => store.state.toparticle);
+
+const count = ref(0);
+
+count.value = metadata.value.totalNum;
 </script>
 
 <style scoped>
 .top_recommend {
   width: 100%;
-  background-color: azure;
+  background-color: #fff;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  padding: 28px 50px;
 }
 .top_recommend::after {
+  /* 等比缩放顶部推荐 */
   content: "";
   padding-top: 15%;
   display: block;
 }
-.text {
-  font-size: 24px;
-  font-weight: 600;
+.top_article_cover {
+  width: 25%;
+  background-color: azure;
+  background-position: center;
+  background-size: cover;
+  flex-shrink: 0;
+}
+.top_article_cover::after {
+  content: "";
+  display: block;
+  padding-top: 56%;
 }
 </style>
